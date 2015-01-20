8b204156599d6638cd1eda34a83e7a46ba009160
Kevin Moore <kevinm@elementaltechnologies.com>
2015-01-20 18:07:02 +0000

    some uncommitted changes

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
 
 void print_usage()
 {
@@ -23,9 +24,55 @@ void read_packets()
 	retcode = read(sd,buffer,sizeof(buffer));
 	packets++;
 
-	if ((packets % 100) == 0) 
+	if ((packets % 1000) == 0) 
 	{
 		printf("%i packets received.\n",packets);
+	}
+
+}
+
+static void recvpacket()
+{
+	static unsigned int packets = 0;
+	struct msghdr msg;
+	struct iovec entry;
+	struct sockaddr_in from_addr;
+	struct {
+		struct cmsghdr cm;
+		char control[512];
+	} control;
+	int res;
+
+	memset(&msg, 0, sizeof(msg));
+	msg.msg_iov = &entry;
+	msg.msg_iovlen = 1;
+	entry.iov_base = buffer;
+	entry.iov_len = sizeof(buffer);
+	msg.msg_name = (caddr_t)&from_addr;
+	msg.msg_namelen = sizeof(from_addr);
+	msg.msg_control = &control;
+	msg.msg_controllen = sizeof(control);
+
+	res = recvmsg(sd, &msg, 0);
+	if (res < 0) {
+		printf("%s %s: %s\n",
+		       "recvmsg",
+		       strerror(errno));
+	}
+
+	
+	packets++;
+
+	if ((packets % 1000) == 0) 
+	{
+		struct cmsghdr *cmsg;
+		printf("%i packets received.\n",packets);
+		for (cmsg = CMSG_FIRSTHDR(&msg);
+	     	 cmsg;
+	         cmsg = CMSG_NXTHDR(&msg, cmsg)) 
+		{
+			printf(" cmsg [len:%zu] [level:%zu] \n", cmsg->cmsg_len, cmsg->cmsg_level);
+		}
 	}
 
 }
@@ -51,8 +98,23 @@ void open_network_connection(char* dest_ip, char* dest_port, char* interface_ip)
 		perror("Failed setting SO_REUSEADDR");
 		exit(1);
 	}
+	else
+	{
+		printf("SO_REUSEADDR has been successfully set.\n");
+	}
 
-	printf("SO_REUSEADDR has been successfully set.\n");
+	int enable = 1;
+	retcode = setsockopt(sd, SOL_SOCKET, SO_TIMESTAMPNS, reinterpret_cast<char*>(&enable) , sizeof(enable));
+	if (retcode < 0)
+	{
+		perror("Failed setting SO_TIMESTAMPNS");
+		exit(1);
+	}
+	else
+	{
+		printf("SO_TIMESTAMPNS has been successfully set.\n");
+	}
+	
 
 	unsigned short port = strtol(dest_port, NULL, 10);
 	struct sockaddr_in localSock;
